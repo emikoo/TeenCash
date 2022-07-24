d70c610f721d84a14750013865227105cc498c15
emikoo <emikoo@users.noreply.github.com>
2022-07-24 23:22:11 +0600

    auth design updated

--- app/src/main/java/com/teenteen/teencash/presentation/ui/fragments/sign/up/SignUpFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.teenteen.teencash.presentation.ui.fragments.sign.up
-
-import androidx.fragment.app.Fragment
-import com.teenteen.teencash.R
-
-class SignUpFragment : Fragment(R.layout.fragment_sign_up)
