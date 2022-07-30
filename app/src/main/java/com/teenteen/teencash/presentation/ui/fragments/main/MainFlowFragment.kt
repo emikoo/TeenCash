e41beea39b990b65a224eb0eb557c3b12c7aae02
emikoo <emikoo@users.noreply.github.com>
2022-07-30 22:33:59 +0600

    added main navigation & fragments of main

--- app/src/main/java/com/teenteen/teencash/presentation/ui/fragments/main/MainFlowFragment.kt
+++ app/src/main/java/com/teenteen/teencash/presentation/ui/fragments/main/MainFlowFragment.kt
@@ -1,8 +1,17 @@
 package com.teenteen.teencash.presentation.ui.fragments.main
 
+import androidx.navigation.ui.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.teenteen.teencash.R
+import com.teenteen.teencash.databinding.FlowFragmentMainBinding
 import com.teenteen.teencash.presentation.base.BaseFlowFragment
 
 class MainFlowFragment : BaseFlowFragment(
-    R.layout.flow_fragment_main, R.id.nav_host_fragment_main
-)
+    R.layout.flow_fragment_main , R.id.nav_host_fragment_main
+) {
+    private val binding by viewBinding(FlowFragmentMainBinding::bind)
+
+    override fun setupNavigation() {
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+}
