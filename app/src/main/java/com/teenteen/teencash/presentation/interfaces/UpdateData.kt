39e06f1e2103579705edd60fa9b78255ed133bba
emikoo <emikoo@users.noreply.github.com>
2022-09-29 14:48:28 +0600

    added update interface

--- app/src/main/java/com/teenteen/teencash/presentation/interfaces/UpdateData.kt
+++ app/src/main/java/com/teenteen/teencash/presentation/interfaces/UpdateData.kt
@@ -3,4 +3,12 @@ package com.teenteen.teencash.presentation.interfaces
 interface UpdateData {
     fun updateCategory()
     fun updatePiggyBank()
+    fun updateStatistics()
+    fun updateMFList()
+    fun updateBSList()
+    fun achieved()
+}
+
+interface UpdateAchievement {
+    fun updateAchievement()
 }
