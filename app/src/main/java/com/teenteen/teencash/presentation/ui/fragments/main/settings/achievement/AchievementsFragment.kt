313b54bdfa899d23344ad0943c2666971e676151
emikoo <emikoo@users.noreply.github.com>
2022-10-01 17:55:41 +0600

    onBackPressed error solved

--- app/src/main/java/com/teenteen/teencash/presentation/ui/fragments/main/settings/achievement/AchievementsFragment.kt
+++ app/src/main/java/com/teenteen/teencash/presentation/ui/fragments/main/settings/achievement/AchievementsFragment.kt
@@ -1,19 +1,19 @@
 package com.teenteen.teencash.presentation.ui.fragments.main.settings.achievement
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.teenteen.teencash.R
 import com.teenteen.teencash.data.model.Category
 import com.teenteen.teencash.databinding.FragmentAchievementsBinding
 import com.teenteen.teencash.presentation.base.BaseFragment
 import com.teenteen.teencash.presentation.extensions.show
 import com.teenteen.teencash.presentation.interfaces.UpdateAchievement
-import com.teenteen.teencash.presentation.ui.fragments.main.settings.SettingsFragment
 import com.teenteen.teencash.presentation.ui.fragments.main.settings.bottom_sheets.AchievementBottomSheet
 import com.teenteen.teencash.view_model.MainViewModel
 
@@ -28,6 +28,8 @@ class AchievementsFragment() : BaseFragment<FragmentAchievementsBinding>(),
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         progressDialog.show()
         viewModel.getAchievements(prefs.getCurrentUserId())
+        val directions = AchievementsFragmentDirections.actionAchievementsFragmentToSettingsFragment()
+        binding.ibBack.setOnClickListener { findNavController().navigate(directions) }
     }
 
     private fun setupRecyclerView() {
@@ -35,7 +37,6 @@ class AchievementsFragment() : BaseFragment<FragmentAchievementsBinding>(),
         binding.recyclerView.adapter = adapter
         if (array.isEmpty()) binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         else binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
-        binding.ibBack.setOnClickListener { findNavController().popBackStack() }
     }
 
     override fun onClickListener(item: Category) {
@@ -51,6 +52,17 @@ class AchievementsFragment() : BaseFragment<FragmentAchievementsBinding>(),
     }
 
     override fun updateAchievement() { viewModel.getAchievements(prefs.getCurrentUserId()) }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    val directions = AchievementsFragmentDirections.actionAchievementsFragmentToSettingsFragment()
+                    findNavController().navigate(directions)
+                }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 
     override fun attachBinding(
         list: MutableList<FragmentAchievementsBinding> ,
