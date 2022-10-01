b70e3c130f0f34a8c6052af1358394c907b226e7
emikoo <emikoo@users.noreply.github.com>
2022-10-01 21:45:38 +0600

    spinned added

--- app/src/main/java/com/teenteen/teencash/presentation/ui/fragments/main/history/HistoryFragment.kt
+++ app/src/main/java/com/teenteen/teencash/presentation/ui/fragments/main/history/HistoryFragment.kt
@@ -1,9 +1,13 @@
 package com.teenteen.teencash.presentation.ui.fragments.main.history
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.teenteen.teencash.R
 import com.teenteen.teencash.data.model.History
 import com.teenteen.teencash.databinding.FragmentHistoryBinding
 import com.teenteen.teencash.presentation.base.BaseFragment
@@ -17,8 +21,10 @@ class HistoryFragment : BaseFragment<FragmentHistoryBinding>() {
 
     override fun setupViews() {
         viewModel = ViewModelProvider(this)[HistoryViewModel::class.java]
+        progressDialog.show()
         viewModel.getHistory(prefs.getCurrentUserId())
         setupRecyclerView()
+        setupSpinner()
     }
 
     private fun setupRecyclerView() {
@@ -26,11 +32,26 @@ class HistoryFragment : BaseFragment<FragmentHistoryBinding>() {
         binding.recyclerView.adapter = adapter
     }
 
+    private fun setupSpinner() {
+        val adapter =
+            ArrayAdapter.createFromResource(requireActivity(), R.array.spinner_date , R.layout.spinner_item)
+
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+        binding.spinner.adapter = adapter
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+            override fun onItemSelected(p0: AdapterView<*>? , p1: View? , p2: Int , p3: Long) {
+//                categoryId = p0?.getItemIdAtPosition(p2).toString().toInt()
+            }
+        }
+    }
+
     override fun subscribeToLiveData() {
         viewModel.history.observe(viewLifecycleOwner, Observer {
             historyArray.clear()
             historyArray.addAll(it)
             adapter.notifyDataSetChanged()
+            progressDialog.dismiss()
         })
     }
 
